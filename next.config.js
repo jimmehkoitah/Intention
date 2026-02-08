64a48c65705f6388d284064d1de4708d167eb083
Jim Koita <jimmehkoitah@gmail.com>
2026-02-08 13:39:26 -0500

    Ready for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,29 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.ggpht.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.ytimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static-cdn.jtvnw.net',
-      },
-    ],
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
